f017dfc447dccdd9a0c0b701cf32a30bb3355561
HaKu <haku@sumemo.dev>
2025-06-30 12:42:39 -0400

    feat(web): use proxy to send request

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,8 @@
 import axios from "axios";
 import type {Zone} from "@/types/zone.ts";
-import type {Fight, MemberFight, MemberZoneProgress} from "@/types/fight.ts";
+import type {Fight, MemberZoneProgress} from "@/types/fight.ts";
 
-const BASE_URL = "https://api.sumemo.dev";
-
-export const getMemberFights = async (name: string): Promise<MemberFight> => {
-    const res = await axios.get(`${BASE_URL}/member/${name}/fight`);
-    return res.data;
-};
+const BASE_URL = "/api";
 
 export const getFightByID = async (id: number): Promise<Fight> => {
     const res = await axios.get(`${BASE_URL}/fight/${id}`);
@@ -26,6 +21,5 @@ export const getZoneNameByID = async (zoneID: number): Promise<string> => {
 
 export const getMemberZoneProgress = async (memberIdentifier: string, zoneID: number): Promise<MemberZoneProgress> => {
     const res = await axios.get<MemberZoneProgress>(`${BASE_URL}/member/${memberIdentifier}/progress/${zoneID}`);
-    console.info(res.data);
     return res.data;
 };
