3e73d1257bd563be261af6c794cfad4002890e38
HaKu <haku@sumemo.dev>
2026-01-07 00:53:24 -0500

    fix: update tooltip label for clarity and remove unused icon code

--- src/components/custom/fight/card/FightCardProgress.tsx
+++ src/components/custom/fight/card/FightCardProgress.tsx
@@ -1,6 +1,3 @@
-import WrapperIcon from "@/components/custom/wrapper/WrapperIcon.tsx";
-import VerifiedIcon from "@/assets/icon/verified.svg?react";
-import SlashIcon from "@/assets/icon/slash.svg?react";
 import { cn } from "@/lib/utils.ts";
 import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip.tsx";
 import { getTextGradient } from "@/lib/gradient.ts";
@@ -21,23 +18,23 @@ export function FightCardProgress({ clear, phaseName, subphaseName, progressHpRe
             <div className={`flex gap-x-1 gap-y-1 items-center justify-end`}>
 
                 {/* clear icon */}
-                {clear ? (
-                        <WrapperIcon icon={VerifiedIcon}
-                                     className={cn(
-                                             "size-5 shrink-0 transition-colors duration-300",
-                                     )}
-                                     primary="var(--primary-ring)"
-                                     secondary="var(--primary-foreground)"
-                        />
-                ) : (
-                        <WrapperIcon icon={SlashIcon}
-                                     className={cn(
-                                             "size-5 shrink-0 transition-colors duration-300",
-                                     )}
-                                     primary="var(--secondary-ring)"
-                                     secondary="var(--secondary-foreground)"
-                        />
-                )}
+                {/*{clear ? (*/}
+                {/*        <WrapperIcon icon={VerifiedIcon}*/}
+                {/*                     className={cn(*/}
+                {/*                             "size-5 shrink-0 transition-colors duration-300",*/}
+                {/*                     )}*/}
+                {/*                     primary="var(--primary-ring)"*/}
+                {/*                     secondary="var(--primary-foreground)"*/}
+                {/*        />*/}
+                {/*) : (*/}
+                {/*        <WrapperIcon icon={SlashIcon}*/}
+                {/*                     className={cn(*/}
+                {/*                             "size-5 shrink-0 transition-colors duration-300",*/}
+                {/*                     )}*/}
+                {/*                     primary="var(--secondary-ring)"*/}
+                {/*                     secondary="var(--secondary-foreground)"*/}
+                {/*        />*/}
+                {/*)}*/}
 
                 <Tooltip>
                     <TooltipTrigger asChild>
@@ -51,7 +48,7 @@ export function FightCardProgress({ clear, phaseName, subphaseName, progressHpRe
                                     <div className="flex gap-x-0.5 items-baseline">
                                         <span className={cn(
                                                 "text-right justify-start text-xs font-medium",
-                                        )}>剩余</span>
+                                        )}>剩余血量</span>
                                         <span className={cn(
                                                 "text-right justify-start text-sm font-mono font-semibold",
                                         )}>{progressHpRemain}</span>
